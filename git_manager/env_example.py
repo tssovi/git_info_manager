9ffb2f5c9a527a97e5487f3e3c2181f85cec4641
Tusamma Sal Sabil <tssovi@gmail.com>
2020-04-19 21:04:13 +0600

    Add env.py example

---
+++ git_manager/env_example.py
@@ -0,0 +1,15 @@
+SECRET_KEY = '#y+o0-^t0xh!h#%7trbg6cjb!h8)nm70=604105nljxx99g#!('
+ALLOWED_HOSTS = ['*']
+DEBUG = True
+
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+DATABASE_NAME = 'database_name'
+DATABASE_USER = 'db_username'
+DATABASE_PASSWORD = 'db_password'
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = '5432'
+
+CLIENT_ID = '6a32d1f72b065695125f'
+CLIENT_SECRET = 'e5a990c7555e25608a8d41b3082d2e78a838322c'
+
+NGROK_URL = 'ngrok_url'
